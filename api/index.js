2ca9a455966c5fb985cf9e231ea7da3695de4066
Marcio Ferreira <hmarciogferreira@gmail.com>
2024-08-09 18:34:45 -0300

    Ajustes 4

--- api/index.js
+++ api/index.js
@@ -8,13 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const lista = [
-    { id: 1, name: 'Song 1', music: 'Artist 1', table: '10' },
-    { id: 2, name: 'Song 2', music: 'Artist 2', table: '10' },
-    { id: 3, name: 'Song 3', music: 'Artist 3', table: '10' },
-    { id: 4, name: 'Song 4', music: 'Artist 4', table: '10' },
-
-];
+const lista = [];
 
 app.get('/', (req, res) => {
     res.sendFile(process.env.INIT_CWD + '/index.html');
@@ -38,17 +32,14 @@ app.put('/musicas/:indice', (req, res) => {
     return res.status(200).json({ message: 'Música Atualizada com Sucesso.'})
 })
 
-app.delete('/musicas/:id', (req, res) => {
+app.delete('/musicas/:indice', (req, res) => {
     const indice = req.params.indice;
     lista.splice(indice, 1);
     return res.status(200).json({ message: 'Música Removida com Sucesso.'})
-})
-
-app.get('/', (req, res) => {
-    res.sendFile('C:/Users/Thaila/Documents/PROJETOS_SOFTWARE/karaoke/index.html');
 })
 
 app.listen(3000, 'localhost', () => {
     console.log('Server is running on port http://localhost:3000');
 });
 
+module.exports = app;
